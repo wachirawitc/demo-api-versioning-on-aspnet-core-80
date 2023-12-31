e10336c7408bfda49f83b0d16cfd03c623d7eb99
wachirawit <wachirawit.chankaew@gmail.com>
2023-12-31 11:29:52 +0700

    Update controller

--- src/Demo/Controllers/V1/ExampleController.cs
+++ src/Demo/Controllers/V1/ExampleController.cs
@@ -5,9 +5,10 @@ namespace Demo.Controllers.V1;
 
 [ApiController]
 [ApiVersion(1.0)]
-[Route("[controller]")]
+[Route("v{version:apiVersion}/[controller]")]
 public class ExampleController : ControllerBase
 {
+    [HttpGet]
     public string Get()
     {
         return "Hello from V1";
--- src/Demo/Controllers/V2/ExampleController.cs
+++ src/Demo/Controllers/V2/ExampleController.cs
@@ -5,9 +5,10 @@ namespace Demo.Controllers.V2;
 
 [ApiController]
 [ApiVersion(2.0)]
-[Route("[controller]")]
+[Route("v{version:apiVersion}/[controller]")]
 public class ExampleController : ControllerBase
 {
+    [HttpGet]
     public string Get()
     {
         return "Hello from V2";
