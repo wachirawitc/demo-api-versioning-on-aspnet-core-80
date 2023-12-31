550cecd684f5cfc3c770144cceacd57d81bf819b
wachirawit <wachirawit.chankaew@gmail.com>
2023-12-31 11:29:40 +0700

    Add new file

---
+++ src/Demo/Controllers/V1/ExampleController.cs
@@ -0,0 +1,15 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers.V1;
+
+[ApiController]
+[ApiVersion(1.0)]
+[Route("[controller]")]
+public class ExampleController : ControllerBase
+{
+    public string Get()
+    {
+        return "Hello from V1";
+    }
+}
---
+++ src/Demo/Controllers/V2/ExampleController.cs
@@ -0,0 +1,15 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers.V2;
+
+[ApiController]
+[ApiVersion(2.0)]
+[Route("[controller]")]
+public class ExampleController : ControllerBase
+{
+    public string Get()
+    {
+        return "Hello from V2";
+    }
+}
