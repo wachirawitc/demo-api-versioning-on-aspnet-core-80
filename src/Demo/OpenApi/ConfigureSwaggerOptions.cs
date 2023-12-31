ad0018602b3271b000b677ce4e0eb3bd14b04eae
wachirawit <wachirawit.chankaew@gmail.com>
2023-12-31 11:30:08 +0700

    Config api versioning

---
+++ src/Demo/OpenApi/ConfigureSwaggerOptions.cs
@@ -0,0 +1,40 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Text;
+
+namespace Demo.OpenApi;
+
+public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+{
+    public void Configure(SwaggerGenOptions options)
+    {
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+        }
+    }
+
+    private static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+    {
+        var text = new StringBuilder("Demo API with OpenAPI, Swashbuckle, and API versioning.");
+
+        var info = new OpenApiInfo();
+        info.Title = "Demo API";
+        info.Version = description.ApiVersion.ToString();
+
+        if (description.IsDeprecated)
+        {
+            text.Append("This API version has been deprecated.");
+        }
+
+        info.Description = text.ToString();
+
+        return info;
+    }
+
+    private readonly IApiVersionDescriptionProvider provider;
+
+    public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
+}
--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -1,17 +1,51 @@
+using Asp.Versioning;
+using Demo.OpenApi;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddApiVersioning(options =>
+    {
+        options.ReportApiVersions = true;
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        options.DefaultApiVersion = new ApiVersion(1, 0);
+        options.ApiVersionReader = new UrlSegmentApiVersionReader();
+    })
+    .AddMvc()
+    .AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
+    });
+
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen();
+builder.Services.TryAddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+
+builder.Services.AddSwaggerGen(
+    options => { options.OperationFilter<SwaggerDefaultValues>(); });
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+
+    app.UseSwaggerUI(
+        options =>
+        {
+            var descriptions = app.DescribeApiVersions();
+            foreach (var description in descriptions)
+            {
+                var url = $"/swagger/{description.GroupName}/swagger.json";
+                var name = description.GroupName.ToUpperInvariant();
+                options.SwaggerEndpoint(url, name);
+            }
+        });
 }
 
 app.UseHttpsRedirection();
